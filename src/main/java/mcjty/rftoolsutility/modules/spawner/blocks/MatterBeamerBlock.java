5a82ea3bae699c85a320668b9a819bbbe9977b35
Nathan Regner <nathanregner@gmail.com>
2025-06-29 10:25:35 -0600

    Allow redstone conduit connections to matter beamer

--- src/main/java/mcjty/rftoolsutility/modules/spawner/blocks/MatterBeamerBlock.java
+++ src/main/java/mcjty/rftoolsutility/modules/spawner/blocks/MatterBeamerBlock.java
@@ -5,16 +5,22 @@ import mcjty.lib.blocks.RotationType;
 import mcjty.lib.builder.BlockBuilder;
 import mcjty.rftoolsbase.tools.ManualHelper;
 import mcjty.rftoolsutility.compat.RFToolsUtilityTOPDriver;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.neoforged.neoforge.common.extensions.IBlockExtension;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
 
 import static mcjty.lib.builder.TooltipBuilder.*;
 
-public class MatterBeamerBlock extends BaseBlock {
+public class MatterBeamerBlock extends BaseBlock implements IBlockExtension {
 
     public MatterBeamerBlock() {
         super(new BlockBuilder()
@@ -35,5 +41,10 @@ public class MatterBeamerBlock extends BaseBlock {
     protected void createBlockStateDefinition(@Nonnull StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
         builder.add(BlockStateProperties.LIT);
+    }
+
+    @Override
+    public boolean canConnectRedstone(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @Nullable Direction direction) {
+        return true;
     }
 }
